b424c547e9cc7d89758242e8ee2b144a2427c5fe
shota <riodeja.5.shota@gmail.com>
2016-05-08 01:04:12 +0900

    recovery miss commit

--- paiza/Contest/D038/Main.scala
+++ paiza/Contest/D038/Main.scala
@@ -1,18 +1,8 @@
 object Main extends App{
 	val sc = new java.util.Scanner(System.in)
-	val t_1 = sc.nextInt
-	val t_2 = sc.nextInt
-	val t_3 = sc.nextInt
-	val t_4 = sc.nextInt
-	val t_5 = sc.nextInt
-	val t_6 = sc.nextInt
-	val t_7 = sc.nextInt
+	val n = sc.nextInt
 
-	val lstT = List(t_1, t_2, t_3, t_4, t_5, t_6, t_7)
+	println(calcGameNum(n))
 
-	println(calcDaysToGoOut(lstT))
-
-	def calcDaysToGoOut(lstT: List[Int]) = {
-		lstT.filter(x => x <= 30).size
-	}
+	def calcGameNum(p1: Int) = p1 * (p1 - 1) / 2
 }
