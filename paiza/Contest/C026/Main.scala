0a91eac806e546b3f77b5122654253d3cc8d3051
shota <riodeja.5.shota@gmail.com>
2016-05-15 00:04:33 +0900

    C026 commit

--- paiza/Contest/C026/Main.scala
+++ paiza/Contest/C026/Main.scala
@@ -1,14 +1,20 @@
 object Main extends App {
 
-    // Input NSp
     val sc = new java.util.Scanner(System.in)
+
+    // Input NSp
     val N,S,p = sc.nextInt
 
     // Input Carrots(m_N s_N)
-    val listMS = readCarrots(Nil, N, 0)
+    var carrots: List[List[Int]] = Nil
+    for (x <- 1 to N) {
+        val m,s = sc.nextInt
+        carrots = List(x, m, s) :: carrots
+    }
+    carrots = carrots.reverse
 
     // calculate Best Carrot Number
-    val carrotNumber = calcBestCarrotNumber(S, p, listMS)
+    val carrotNumber = calcBestCarrotNumber(S, p, carrots)
 
     // Output Result
     if (carrotNumber > 0) {
@@ -17,14 +23,9 @@ object Main extends App {
         println("not found")
     }
     
-    def readCarrots(carrots: List[List[Int]], N: Int, n: Int): List[List[Int]] = (n) match {
-        case N => carrots.reverse
-        case _ => readCarrots((n :: readLine.split(" ").map(_.toInt).toList) :: carrots, N, n + 1)
-    }
-    
-    def calcBestCarrotNumber(S: Int, p: Int, listMS: List[List[Int]]): Int = {
-        listMS.filter(x => ((S - p) <= x(2) && x(2) <= (S + p))) match {
-            case listFiltered: List[List[Int]] if (listFiltered.size > 0) => listFiltered.maxBy(_(1)).apply(0) + 1
+    def calcBestCarrotNumber(S: Int, p: Int, carrots: List[List[Int]]): Int = {
+        carrots.filter(x => ((S - p) <= x(2) && x(2) <= (S + p))) match {
+            case listFiltered: List[List[Int]] if (listFiltered.size > 0) => listFiltered.maxBy(_(1)).apply(0)
             case _ => 0
         }
     }
