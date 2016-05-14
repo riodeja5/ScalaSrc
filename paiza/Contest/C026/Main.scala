e874504a3baec06140cf1d00f0a14b1a65bcf764
shota <riodeja.5.shota@gmail.com>
2016-05-14 10:15:46 +0900

    I modified C026 Code, But paiza generates NullPointerException

--- paiza/Contest/C026/Main.scala
+++ paiza/Contest/C026/Main.scala
@@ -1,34 +1,31 @@
 object Main extends App {
 
+    // Input NSp
     val sc = new java.util.Scanner(System.in)
     val N,S,p = sc.nextInt
- //   val listMS = List.fill(N, 2)(sc.nextInt)
+
+    // Input Carrots(m_N s_N)
     val listMS = readCarrots(Nil, N, 0)
 
+    // calculate Best Carrot Number
+    val carrotNumber = calcBestCarrotNumber(S, p, listMS)
 
-    println(listMS)
-
-    println(calcBestCarrotNumber(S, p, listMS))
-
-
-    /*
-    for (x <- 1 to N) {
-        val m = sc.nextInt
-        val s = sc.nextInt
-        val carrot = readLine.split(" ").map(_.toInt)
-        carrot(2)
+    // Output Result
+    if (carrotNumber > 0) {
+        println(carrotNumber)
+    } else {
+        println("not found")
     }
-
-    println(listMS)
-    */
     
     def readCarrots(carrots: List[List[Int]], N: Int, n: Int): List[List[Int]] = (n) match {
         case N => carrots.reverse
         case _ => readCarrots((n :: readLine.split(" ").map(_.toInt).toList) :: carrots, N, n + 1)
     }
     
-    def calcBestCarrotNumber(S: Int, p: Int, listMS: List[List[Int]]): List[List[Int]] = {
-        listMS.map(e => e.filter(x => (S - p) <= x && x <= (S + p)))
-//        .maxBy(_(1))
+    def calcBestCarrotNumber(S: Int, p: Int, listMS: List[List[Int]]): Int = {
+        listMS.filter(x => ((S - p) <= x(2) && x(2) <= (S + p))) match {
+            case listFiltered: List[List[Int]] if (listFiltered.size > 0) => listFiltered.maxBy(_(1)).apply(0) + 1
+            case _ => 0
+        }
     }
 }
